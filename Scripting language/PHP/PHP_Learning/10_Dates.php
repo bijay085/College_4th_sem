c6c761844707c2636f15d410e7d2628872bc036b
bijay085 <bijaykoirala162@gmail.com>
2024-03-02 10:58:46 +0545

    php

--- Scripting language/PHP/PHP_Learning/10_Dates.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "l -> day <br> Y -> year <br> s -> second <br> m -> minutes <br> g and i -> time";
-echo "Today date is :" . date('l Y m g:i:s A');
-
-echo "<br>";
-echo "Copyright " . date("l")  . "  " . " || All right reserved";
-
-?>
